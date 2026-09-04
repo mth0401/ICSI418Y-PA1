4e365a7a3193ce0f67c5cb7da775a5011eae84c3
Matthew Higgins <mth0401@gmail.com>
2026-09-04 17:26:17 -0400

    Added color classes for priority and colors for other elements. Added Submit button function in js

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-onSubmit() {
-    
+const submit = document.querySelector("button");
+const tasks = [];
+function onSubmit(event) {
+    event.preventDefault();
+    const task = document.querySelector("#task-input").value;
+    const priority = document.querySelector("#priority").value;
+    const div = document.querySelector("#task-list");
+    const par = document.createElement("p");
+
+    if(task.trim() == "") {
+        return console.log("Task must have a name");
+    }
+    else {
+        par.textContent = form;
+        div.appendChild(par);
+    }
+
+    if (priority == "low") {
+        par.classList.add("Low");
+    }
+    else if (priority == "medium") {
+        par.classList.add("Medium");
+    }
+    else {
+        par.classList.add("High");
+    }
+
+    tasks.push(
+        {
+            name: task,
+            priority: priority,
+            completed: par.classList.contains("Completed")
+        }
+    );
 }
+
+function onTaskCLick() {
+
+}
+
+
+submit.addEventListener("click", onSubmit);
