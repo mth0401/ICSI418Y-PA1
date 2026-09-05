8e59bc003968f38175f0e119a0f23e17dab21c4f
Matthew Higgins <mth0401@gmail.com>
2026-09-04 20:21:02 -0400

    Finished HTML, CSS, JS. Need to start README

--- script.js
+++ script.js
@@ -1,42 +1,74 @@
 const submit = document.querySelector("button");
 const tasks = [];
+const showTask = document.querySelector("#show-tasks");
+const div = document.querySelector("#task-list");
+
 function onSubmit(event) {
     event.preventDefault();
-    const task = document.querySelector("#task-input").value;
+    let task = document.querySelector("#task-input").value;
     const priority = document.querySelector("#priority").value;
-    const div = document.querySelector("#task-list");
-    const par = document.createElement("p");
 
     if(task.trim() == "") {
         return console.log("Task must have a name");
     }
-    else {
-        par.textContent = form;
-        div.appendChild(par);
-    }
-
-    if (priority == "low") {
-        par.classList.add("Low");
-    }
-    else if (priority == "medium") {
-        par.classList.add("Medium");
-    }
-    else {
-        par.classList.add("High");
-    }
-
+    
     tasks.push(
         {
             name: task,
             priority: priority,
-            completed: par.classList.contains("Completed")
+            completed: false,
+            deleted: false
         }
     );
+
+    task.textContent = "";
 }
 
-function onTaskCLick() {
+function displayTasks(event) {
+    event.preventDefault();
+    div.textContent = "";
+    tasks.forEach(element => {
+        if (!element.deleted) {
+            const par = document.createElement("p");
+            const compButt = document.createElement("button");
+            const delButt = document.createElement("button")
+            compButt.textContent = "Complete";
+            delButt.textContent = "Delete";
+            par.textContent = "Task: " + element.name + "   || Priority: " + element.priority;
+            document.querySelector("#task-list").appendChild(par);
+            document.querySelector("#task-list").appendChild(compButt);
+            document.querySelector("#task-list").appendChild(delButt);
 
+            if (element.priority == "low") {
+                par.classList.add("Low");
+            }
+            else if (element.priority == "medium") {
+                par.classList.add("Medium");
+            }
+            else {
+                par.classList.add("High");
+            }
+            compButt.addEventListener("click", function () {
+                if (!element.completed) {
+                    par.classList.add("Completed");
+                    element.completed = true;
+                }
+                else {
+                    par.classList.remove("Completed");
+                    element.completed = false;
+                }
+            });
+
+            delButt.addEventListener("click", function() {
+                par.remove();
+                compButt.remove();
+                delButt.remove();
+                element.deleted = true;
+            });
+        }
+        
+    });
 }
-
 
 submit.addEventListener("click", onSubmit);
+showTask.addEventListener("click", displayTasks);
